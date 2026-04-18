b24ebd49b0cd047c5942f22fd8e375eefaf76457
Victoria2328 <vhuertasortiz@gmail.com>
2026-04-17 20:00:25 -0500

    errores corregidos en el registro
    errores corregidos en el registro

--- aviara_v2/usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/productos/', views.productos_api, name='productos_api'),
-    path('lista/', views.lista_usuarios, name='lista_usuarios'),
-    path('editar/<int:pk>/', views.editar_usuario, name='editar_usuario'),
-    path('eliminar/<int:pk>/', views.eliminar_usuario, name='eliminar_usuario'),
-]
